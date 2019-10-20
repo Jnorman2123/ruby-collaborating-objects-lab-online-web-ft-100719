68772098781db553d6061fe0efd9c6d805579043
Jonathan Norman <jnorman2123@gmail.com>
2019-10-20 23:42:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,9 @@ class Song
   def self.new_by_filename(filename)
     split_file = filename.split(" - ")
     song = self.new(split_file[1])
-    artist = Artist.find_or_create_by_name(split_file[0])
-    song.artist = artist
+    song.artist = split_file[0]
+    # artist = Artist.find_or_create_by_name(split_file[0])
+    # song.artist = artist
     artist.add_song(song)
     song
   end
