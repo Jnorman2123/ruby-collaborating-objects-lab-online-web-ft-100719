f65d61c44b4686c70a22cd12111e6e3f8c914c28
Jonathan Norman <jnorman2123@gmail.com>
2019-10-20 22:55:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,7 +16,7 @@ class Artist
   end
 
   def songs
-    artist_songs = Song.all.select { |song| song.artist == self}
+    Song.all.select { |song| song.artist == self}
   end
 
   def self.find_or_create_by_name(name)
