abb8e7ce76d4e0dc9975acbd95a43b2866398924
Jonathan Norman <jnorman2123@gmail.com>
2019-10-20 18:09:13 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    song - self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end
+
+  def artist_name=()
+
+  end
+end
