38f8ccecc3b36bc8586442f5962798f24cdeaab0
Jonathan Norman <jnorman2123@gmail.com>
2019-10-20 23:05:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,8 +6,8 @@ class MP3Importer
   end
 
   def files
-    Song.new(path.split(" - ")[1])
-    Song.artist.name = path.split(" - ")[0]
+    new_song = Song.new(path.split(" - ")[1])
+    new_song.artist.name = path.split(" - ")[0]
   end
 
   def import(list_of_filenames)
