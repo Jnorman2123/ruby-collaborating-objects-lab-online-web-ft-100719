61628b358f7d1faf7f68403ed34e7984cebc2e77
Jonathan Norman <jnorman2123@gmail.com>
2019-10-21 00:02:27 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,7 +10,7 @@ class MP3Importer
     @files.delete_if {|file| file == "." || file == ".."}
   end
 
-  def import(list_of_filenames)
+  def import
     list_of_filenames.each do |filename|
       Song.new_by_filename(filename)
       Arist.find_or_create_by_name(filename)
